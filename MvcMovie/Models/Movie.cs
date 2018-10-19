224d1a74132e2ee4c31e065df2cda13bebde5dab
Ashiq <ashiq08401@gmail.com>
2018-10-19 18:46:33 -0400

    MVC Tutorial

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,16 +10,30 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
+   
         public int ID { get; set; }
+
+
         public string Title { get; set; }
 
+        
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMMMM d, yyyy}")]
+        [Required(ErrorMessage = "Please enter a valid date")]
         public DateTime ReleaseDate { get; set; }
+
+
         public string Genre { get; set; }
 
+
+   
         [Column(TypeName = "decimal(18, 2)")]
+
+
+        [Required(ErrorMessage = "Please enter a valid price")]
         public decimal Price { get; set; }
- 
+
     }
+
 }
